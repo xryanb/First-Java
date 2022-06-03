351ae03a822e646e6ac7fa2e81cc86ca92dd0f28
ryan bautista <96922150+xryanb@users.noreply.github.com>
2022-06-03 13:13:37 -0700

    Add files via upload

---
+++ FirstJava.java
@@ -0,0 +1,5 @@
+public class FirstJava {
+    public static void main(String[] args){
+        System.out.println("My name is Ryan B \n" + "I am 28 years old \n" + "My hometown is Long Beach");
+    }
+}
